72bdcf41b8d7626a72ff8417a7058fcb350ae899
gehu <gehu1107@gmail.com>
2014-07-16 14:30:57 +0800

    test

--- test/test.go
+++ test/test.go
@@ -2,9 +2,19 @@ package main
 
 import "fmt"
 
-func main(){
-v1 := []int{2, 3}
+func main() {
+	v1 := []int{0, 1, 2, 3, 4, 5}
 	fmt.Println(v1)
-	v1[0], v1[1] = v1[1], v1[0]
-	fmt.Println(v1)
+	var index int
+	index = 1
+	v1 = append(v1[:index-1], v1[index+1:]...)
+	fmt.Println(v1, "= [1, 2, 3, 4, 5]")
+
+	index = 4
+	v1 = append(v1[:index-1], v1[index+1:]...)
+	fmt.Println(v1, "= [1, 2, 3, 4]")
+
+	index = 2
+	v1 = append(v1[:index-1], v1[index+1:]...)
+	fmt.Println(v1, "= [1, 2, 4, 5]")
 }
